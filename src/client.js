6723b4b9a11f71b7003e86ed6d740c31091b091e
Guillermo Lopez <guillermolopez2529@gmail.com>
2017-04-30 20:53:58 -0400

    Client render added

--- src/client.js
+++ src/client.js
@@ -3,11 +3,9 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Pages from './pages/containers/Pages';
 
-const container = document.querySelector('.App-container');
-
 render(
   <BrowserRouter>
     <Pages />
   </BrowserRouter>,
-  container
+  document.querySelector('.App-container')
 );
